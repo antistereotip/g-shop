ee8d548c1a6fab7218f665e6e8f4608885aef20c
hightech <hightech@root.tech.net>
2015-09-24 21:42:25 +0200

    moved api url

--- settings.php
+++ settings.php
@@ -3,20 +3,11 @@
 global $settings;
 
 $settings = array();
-$settings['api_url'] = 'http://imba.local/html/hightech/autoupdate/server/api/';
 $settings['db_engine'] = 'mysql';
 $settings['db_host'] = 'localhost';
 $settings['db_database'] = 'hightech';
 $settings['db_user'] = 'hightech';
 $settings['db_pass'] = 'hightech124';
 $settings['debug'] = true;
-/*
-$settings['api_url'] = 'http://localhost/wptests/';
-$settings['db_engine'] = 'mysql';
-$settings['db_host'] = 'localhost';
-$settings['db_database'] = 'wptests';
-$settings['db_user'] = 'wptests';
-$settings['db_pass'] = 'wptests124';
-*/
 
 ?>
